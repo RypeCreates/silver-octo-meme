0c66ecc90fdf139ed02968b15c7a4849a74838ac
Ryan Petit <ryanpetit@Ryans-MacBook-Pro.local>
2020-01-21 14:38:25 -0500

    Fixed navigation for categories list

--- silver-octo-app/silver-octo-app/Controllers/CategoriesController.cs
+++ silver-octo-app/silver-octo-app/Controllers/CategoriesController.cs
@@ -12,6 +12,19 @@ namespace silver_octo_app.Controllers
 {
     public class CategoriesController : Controller
     {
+
+        // TODO: Remove CategoryList field after relational database setup.
+        private List<Category> CategoryList = new List<Category>
+            {
+                new Category { Name = "Coffee", Id = 0, Description = "The good stuff!" },
+                new Category { Name = "Groceries", Id = 1, Description = "Food and things." },
+                new Category { Name = "cat3", Id = 2, Description = "" },
+                new Category { Name = "cat4", Id = 3, Description = "" },
+                new Category { Name = "cat5", Id = 4, Description = "" },
+                new Category { Name = "cat6", Id = 5, Description = "" },
+                new Category { Name = "cat7", Id = 6, Description = "" }
+            };
+
         // GET: Categories/Random
         [Route("categories/Random")]
         public ActionResult Random()
@@ -36,11 +49,7 @@ namespace silver_octo_app.Controllers
         [Route("categories/list")]
         public ActionResult ListCategories()
         {
-            var categories = new List<Category>
-            {
-                new Category { Name = "Coffee" },
-                new Category { Name = "Groceries" }
-            };
+            var categories = CategoryList;
 
             var viewModel = new ListCategoriesViewModel
             {
@@ -48,6 +57,21 @@ namespace silver_octo_app.Controllers
             };
 
             return View(viewModel);
+        }
+
+        // GET: Categories/Id
+        [Route("categories/{id}")]
+        public ActionResult Index(int Id)
+        {
+            return View(GetById(Id));
+        }
+
+
+        // Private Methods
+
+        private Category GetById(int Id)
+        {
+            return CategoryList.FirstOrDefault(x => x.Id == Id);
         }
     }
 
